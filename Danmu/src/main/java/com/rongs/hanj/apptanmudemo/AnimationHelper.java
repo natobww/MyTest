276a378e1f4b8bc672386849a148ccae0003f548
natobww <natobww@163.com>
2017-02-15 16:32:27 +0800

    添加弹幕项目

---
+++ Danmu/src/main/java/com/rongs/hanj/apptanmudemo/AnimationHelper.java
@@ -0,0 +1,40 @@
+package com.rongs.hanj.apptanmudemo;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.TranslateAnimation;
+
+/**
+ * 动画工具类
+ * Created by hanj on 15-6-4.
+ */
+public class AnimationHelper {
+    /**
+     * 创建水平方向的水平移动画
+     */
+    public static Animation createHorizontalTranslateAnim(Context context, int fromX, int toX) {
+        TranslateAnimation tlAnim = new TranslateAnimation(fromX, toX, 0, 0);
+        //自动计算时间
+        long duration = (long) (Math.abs(toX - fromX) * 1.0f / ScreenUtils.getScreenW(context) * 10000);
+        tlAnim.setDuration(duration);
+        tlAnim.setInterpolator(new LinearInterpolator());
+        tlAnim.setFillAfter(true);
+        return tlAnim;
+    }
+
+
+    /**
+     * 创建垂直方向的平移动画
+     */
+    public static Animation  createVerticalTranslateAnim(Context context, int fromY, int toY) {
+        TranslateAnimation tlAnim = new TranslateAnimation(0, 0, fromY, toY);
+        //自动计算时间
+        long duration = (long) (Math.abs(toY - fromY) * 1.0f / ScreenUtils.getScreenH(context) * 10000);
+        tlAnim.setDuration(duration);
+        tlAnim.setInterpolator(new LinearInterpolator());
+        tlAnim.setFillAfter(true);
+        return tlAnim;
+    }
+}
